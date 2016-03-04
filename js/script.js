7eb84598fcc234ffeb5bab1ad074b68ab4df35a9
qbscarlie <qbscarlie@gmail.com>
2016-03-03 19:12:20 -0600

    now it done

--- js/script.js
+++ js/script.js
@@ -13,18 +13,20 @@ $(document).ready(function() {
 
 });
 
-function loadData(s) {
+function loadData() {
 
 	// 2.
 	// Write an AJAX call here to load your data.
 	// Then PASS the data to writeTable();
 	$.getJSON("js/donations_over_5k.json", function(data) {
-		writeTable(data);
+		//writeTable(data);
+		theData=data;
+		writeTable();
 	});
 }
 
 
-function writeTable(data) {
+function writeTable() {
 
 	// 3.
 	// Make a list of every donation made to "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL"
@@ -33,19 +35,17 @@ function writeTable(data) {
 	// - Contribution Date
 	// - Contributon Information
 	// - Amount
-	for(i=0; i<data.length; i++) {
-		var committee = data[i].Committee;
-		var date = data[i]["Contribution Date"];
-		var info = data[i]["Contribution Information"];
-		var amount = data[i]["Amount"];
+	for(i=0; i<theData.length; i++) {
+		var committee = theData[i].Committee;
+		var date = theData[i]["Contribution Date"];
+		var info = theData[i]["Contribution Information"];
+		var amount = theData[i]["Amount"];
 		if(committee === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL")
-			$(".chart").append(
-				"<div class="table">
-				<tr><div class="donations">
-				<td>"+date+"</td><td>"+info+"</td><td>"+amount+"</td>
-				</div></tr>
-				</div>"
+		{
+			$(".table.donations tbody").append(
+				"<tr><td>"+date+"</td><td>"+info+"</td><td>"+amount+"</td></tr>"
 				);
+		}
 	}
 }
 
